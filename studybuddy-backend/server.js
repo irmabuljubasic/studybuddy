d3246513a19eefe71edfb9d44a329f0190fed62d
sxwkl <sahra.winkler@gmx.at>
2025-04-15 17:40:45 +0200

    backend veränderung

--- studybuddy-backend/server.js
+++ studybuddy-backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js"; // <-- wichtig
+import authRoutes from "./routes/authRoutes.js"; 
 
 dotenv.config();
 
